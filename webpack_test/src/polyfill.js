4f7ff29629cb77065372c9e857658f557ce76bde
Weiguangyi0829 <wgy7413@163.com>
2020-07-06 23:48:00 +0800

    polyfill

--- webpack_test/src/polyfill.js
+++ webpack_test/src/polyfill.js
@@ -1,3 +1,5 @@
-String.protatype.trem = String.protatype.trem || function () {
-    this.replace(/^\s+|\s+$/g, '')//this是String,去双边空格
+if (!String.protatype.trem) {
+    String.protatype.trem = function () {
+        return this.replace(/^\s+|\s+$/g, '')//this是String,去双边空格
+    }
 }

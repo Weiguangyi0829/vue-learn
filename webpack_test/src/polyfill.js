986b5111390a18891ed5b50912c969b7c433f81a
Weiguangyi0829 <wgy7413@163.com>
2020-07-06 23:43:46 +0800

    new

---
+++ webpack_test/src/polyfill.js
@@ -0,0 +1,3 @@
+String.protatype.trem = String.protatype.trem || function () {
+    this.replace(/^\s+|\s+$/g, '')//this是String,去双边空格
+}
--- webpack_test/webpack-config.js
+++ webpack_test/webpack-config.js
@@ -1,30 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-    mode:'development',
-    entry:'./src/main.js',
-    output:{
-        path:__dirname + '/dist',
-        filename:'main.[hash].bundle.js'
+    mode: 'development',
+    entry: {
+        polyfill:'./src/polyfill.js',//兼容性先执行
+        main:'./src/main.js',
         
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].[hash].bundle.js'//name表示entry里的参数
+
+    },
+    resolve:{
+        extensions:['.js','.css'] //不用写扩展名
     },
 
     module: {
-        rules:[
+        rules: [
             {
-                test:/\.css$/g,
-                use:['style-loader','css-loader']
+                test: /\.css$/g,
+                use: ['style-loader', 'css-loader']
             },
             {
-                test:/\.(png|jpg|gif)$/g,
-                use:['file-loader']
+                test: /\.(png|jpg|gif)$/g,
+                use: ['file-loader']
             }
-            
+
         ]
     },
-    plugins:[
+    plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            path:'./dist',
-            template:'./src/utils/index.template.html'
+            path: './dist',
+            template: './src/index.template.html'
         })
+        //更新html和main.js
     ]
 };
